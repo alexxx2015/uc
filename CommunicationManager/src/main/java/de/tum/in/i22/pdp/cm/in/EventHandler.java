cf827078e1ed487748144237cc089dc13d37a5d0
Aleksandar Stoimenov <alex.stoimenov@gmail.com>
2013-06-19 23:26:15 +0200

    Event handler pause/resume implemented

--- CommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/EventHandler.java
+++ CommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/EventHandler.java
@@ -9,7 +9,6 @@ import java.util.concurrent.BlockingQueue;
 
 import org.apache.log4j.Logger;
 
-import de.tum.in.i22.pdp.cm.in.pmp.PmpRequest;
 import de.tum.in.i22.pdp.datatypes.IEvent;
 import de.tum.in.i22.pdp.datatypes.IResponse;
 import de.tum.in.i22.pdp.datatypes.basic.ResponseBasic;
@@ -20,6 +19,8 @@ public class EventHandler implements Runnable {
 	private static EventHandler _instance = null;
 	private BlockingQueue<RequestWrapper> _eventQueue = null;
 	
+	private boolean _pauseWanted = false;
+	private Object _pauseWantedLock = new Object();
 	private boolean _pause = false;
 	private Object _pauseLock = new Object();
 	
@@ -39,10 +40,14 @@ public class EventHandler implements Runnable {
 	
 	/**
 	 * Causes the thread to stop processing the events in the queue.
+	 * It blocks until it is sure the handler has been stopped.
 	 */
 	public void pause() {
 		_logger.info("Pause Event Handler");
-		_pause = true;
+		synchronized(_pauseWantedLock) {
+			_pauseWanted = true;			
+		}
+		checkIfPauseWantedAndWait();
 	}
 	
 	/**
@@ -50,6 +55,11 @@ public class EventHandler implements Runnable {
 	 */
 	public void resume() {
 		_logger.info("Resume Event Handler");
+		synchronized(_pauseWantedLock) {
+			_pauseWanted = false;
+			_pauseWantedLock.notifyAll();
+		}
+		
 		synchronized (_pauseLock) {
 			_pause = false;
 			_pauseLock.notifyAll();
@@ -57,9 +67,14 @@ public class EventHandler implements Runnable {
 	}
 	
 	public void addEvent(IEvent event, IForwarder responder) throws InterruptedException {
+		// wait if there is pause request
+		checkIfPauseWantedAndWait();
+		
+		// wait until the handler is paused
+		checkIfPausedAndWait();
+		
 		// add event to the tail of the queue
 		// put method blocks until the space in the queue is available
-		
 		RequestWrapper obj = new RequestWrapper(event, responder);
 		_logger.debug("Add " + obj + " pair to the queue.");
 		_eventQueue.put(obj);
@@ -82,19 +97,47 @@ public class EventHandler implements Runnable {
 			responder.forwardResponse(response);
 			
 			// now check if there is a request to pause the thread
-			checkForPaused();
+			// if the queue is empty
+			// If the queue is not empty, all the events will be
+			// processed first.
+			// Meanwhile the queue won't accept new events. The client
+			// threads will wait.
+			if (_eventQueue.isEmpty() && _pauseWanted) {
+				synchronized(_pauseWantedLock) {
+					_pauseWanted = false;
+					_pause = true;
+					_pauseWantedLock.notifyAll();
+				}
+			}
+			
+			checkIfPausedAndWait();
 		}
 		
 		// the thread is interrupted, stop processing the events
 	}
 	
-	private void checkForPaused() {
+	private void checkIfPausedAndWait() {
 		synchronized(_pauseLock) {
 			while (_pause) {
 				try {
 					_logger.info("Invoke wait");
 					_pauseLock.wait();
 					_logger.info("After wait");
+				} catch (InterruptedException e) {
+					_logger.error("Event handler interrupted.", e);
+					return;
+				}
+			}
+		}
+	}
+	
+	private void checkIfPauseWantedAndWait() {
+		synchronized(_pauseWantedLock) {
+			while (_pauseWanted) {
+				try {
+					_logger.trace("Invoke wait on PauseWanted");
+					_pauseWantedLock.wait();
+					_logger.trace("After wait on PauseWanted");
 				} catch (InterruptedException e) {
 					_logger.error("Event handler interrupted.", e);
 					return;
