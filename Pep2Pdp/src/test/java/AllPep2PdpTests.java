c2702e8fa05f012f43ba2e1cf02da5a01572e34f
Aleksandar Stoimenov <alex.stoimenov@gmail.com>
2013-06-27 11:17:40 +0200

    Module modular project configured

--- Pep2Pdp/src/test/java/AllPep2PdpTests.java
+++ Pep2Pdp/src/test/java/AllPep2PdpTests.java
@@ -3,11 +3,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import pdp.tests.AllPdpTests;
-
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllPdpTests.class, TestPep2PdpCommunication.class })
+@SuiteClasses({ TestPep2PdpCommunication.class })
 public class AllPep2PdpTests {
 
 }
--- Pmp2Pdp/src/test/java/AllPmpToPdpTests.java
+++ Pmp2Pdp/src/test/java/AllPmpToPdpTests.java
@@ -3,11 +3,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import pdp.tests.AllPdpTests;
-
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllPdpTests.class, TestPmp2PdpCommunication.class })
+@SuiteClasses({ TestPmp2PdpCommunication.class })
 public class AllPmpToPdpTests {
 
 }
