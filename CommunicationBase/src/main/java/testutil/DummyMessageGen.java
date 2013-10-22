6bf8d467eb95d0bbd0795220d46dfa7b372b01c5
Aleksandar Stoimenov <#>
2013-10-22 15:28:29 +0200

    pdp with dummy pdp handler

--- CommunicationBase/src/main/java/testutil/DummyMessageGen.java
+++ CommunicationBase/src/main/java/testutil/DummyMessageGen.java
@@ -71,6 +71,29 @@ public class DummyMessageGen {
 		return response;
 	}
 	
+	public static IResponse createRandomResponse() {
+		EStatus eStatus = null;
+		if (Math.random() > 0.5) { 
+			eStatus = EStatus.ALLOW;
+		} else {eStatus = EStatus.INHIBIT;}
+		
+		IStatus status = new StatusBasic(eStatus, null);
+		
+		List<IEvent> executeActions = new ArrayList<IEvent>();
+		Map<String, String> map1 = new HashMap<String, String>();
+		map1.put("key1", "val1");
+		map1.put("key2", "val2");
+		IEvent action1 = _factory.createEvent("event1", map1);
+		IEvent action2 = _factory.createEvent("event2", map1);
+		executeActions.add(action1);
+		executeActions.add(action2);
+		
+		IEvent modifiedEvent = _factory.createEvent("eventModified", map1);
+		
+		ResponseBasic response = new ResponseBasic(status, executeActions, modifiedEvent);
+		return response;
+	}
+	
 	public static IStatus createAllowStatus() {
 		return new StatusBasic(EStatus.ALLOW);
 	}
