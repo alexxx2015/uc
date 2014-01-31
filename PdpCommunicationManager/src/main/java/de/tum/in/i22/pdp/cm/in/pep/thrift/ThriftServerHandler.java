fdc59cb1d82385fab0a922f405e8eb35e0972931
lovat <lovat@cs.tum.edu>
2014-01-31 16:13:07 +0100

    updated handling of async events

--- PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/pep/thrift/ThriftServerHandler.java
+++ PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/pep/thrift/ThriftServerHandler.java
@@ -20,7 +20,7 @@ public class ThriftServerHandler extends ClientConnectionHandler implements
 
 	private static RequestHandler requestHandler = RequestHandler.getInstance();
 	private static final String IP = "localhost";
-	private static int PORT=8090;
+	private static int PORT = 8090;
 
 	public ThriftServerHandler(int pepPort) {
 		// TODO Auto-generated constructor stub
@@ -28,7 +28,7 @@ public class ThriftServerHandler extends ClientConnectionHandler implements
 		super(null);
 		PORT = pepPort;
 		requestHandler = RequestHandler.getInstance();
-		
+
 	}
 
 	@Override
@@ -40,23 +40,9 @@ public class ThriftServerHandler extends ClientConnectionHandler implements
 	private static final Logger _logger = Logger
 			.getLogger(ThriftServerHandler.class);
 
-	@Override
-	public void processEventAsync(Event e) throws TException {
-		// TODO Auto-generated method stub
-		_logger.debug("processEventAsync invoked, but not supported");
-		// Not needed
-	}
-
-	@Override
-	public Response processEventSync(Event e) throws TException {
-		if (e == null) {
-			_logger.error("received null event, replying with null response");
+	private IResponse processEvent(IEvent ev) {
+		if (ev == null)
 			return null;
-		}
-
-		IEvent ev = new EventBasic(e.name, e.parameters, false);
-		_logger.trace("PDP received thrift event " + e.name);
-		
 		IMessageFactory mf = MessageFactoryCreator.createMessageFactory();
 
 		Object responseObj;
@@ -72,32 +58,70 @@ public class ThriftServerHandler extends ClientConnectionHandler implements
 
 		if (responseObj instanceof IResponse) {
 			IResponse response = (IResponse) responseObj;
-			_logger.trace("Response to return: " + response);
-
-			Response finalThriftResponse;
-
-			switch (response.getAuthorizationAction().getEStatus()) {
-			case INHIBIT:
-				finalThriftResponse = new Response(StatusType.INHIBIT);
-
-			case ALLOW:
-				finalThriftResponse = new Response(StatusType.ALLOW);
-
-			case MODIFY:
-				// TODO: Add modification action cause it is not supported on
-				// the PEP side yet
-				finalThriftResponse = new Response(StatusType.MODIFY);
-			default:
-				finalThriftResponse = new Response(StatusType.ERROR);
-				finalThriftResponse.setComment("Error. Answer is " + response);
-			}
-			throwAwayResponse();
-			return finalThriftResponse;
-
+			return response;
 		} else {
+			_logger.error("Response is not an instance of IResponse. returning null");
 			throw new RuntimeException("IResponse type expected for "
 					+ responseObj);
 		}
+	}
+
+	@Override
+	/****
+	 * Async events are not blocking on the PEP side, therefore they can only be actual events.
+	 * 
+	 */
+	public void processEventAsync(Event e) throws TException {
+		if (e == null) {
+			_logger.error("received null event, replying with null response");
+			return;
+		}
+
+		IEvent ev = new EventBasic(e.name, e.parameters, true);
+		_logger.trace("PDP received asynchronous thrift event " + e.name);
+		processEvent(ev);
+		return;
+	}
+
+	@Override
+	/****
+	 * Async events are not blocking on the PEP side, therefore they can only be actual events.
+	 * As of now, we assume the sync (=blocking) events are ONLY DESIRED events, but for synchronization issues it may be the case that we need to make also the actual events synchronous.
+	 * Note that, however, the events are processed in the same order in which they are received by the queue.
+	 * This means that even if the actual event is not blocking, all the actual events will be processed in the same order they have been received and in case of a desired event, th event won't be allowed to be exectued until all the async events before have been processed
+	 */
+	public Response processEventSync(Event e) throws TException {
+		if (e == null) {
+			_logger.error("received null event, replying with null response");
+			return null;
+		}
+
+		IEvent ev = new EventBasic(e.name, e.parameters, false);
+		_logger.trace("PDP received synchronous thrift event " + e.name);
+
+		IResponse response = processEvent(ev);
+
+		_logger.trace("Response to return: " + response);
+
+		Response finalThriftResponse;
+
+		switch (response.getAuthorizationAction().getEStatus()) {
+		case INHIBIT:
+			finalThriftResponse = new Response(StatusType.INHIBIT);
+
+		case ALLOW:
+			finalThriftResponse = new Response(StatusType.ALLOW);
+
+		case MODIFY:
+			// TODO: Add modification action cause it is not supported on
+			// the PEP side yet
+			finalThriftResponse = new Response(StatusType.MODIFY);
+		default:
+			finalThriftResponse = new Response(StatusType.ERROR);
+			finalThriftResponse.setComment("Error. Answer is " + response);
+		}
+		throwAwayResponse();
+		return finalThriftResponse;
 	}
 
 	@Override
