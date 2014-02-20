095177b1c71d50a895d8ddf0185aaaa692f19761
alex <alex@alex-virtual-machine.(none)>
2014-02-20 14:39:00 +0100

    Fixed EventMatch.eventMatches

--- PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/injection/PdpModuleMockTestPip.java
+++ PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/injection/PdpModuleMockTestPip.java
@@ -4,14 +4,17 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
 import de.tum.in.i22.pdp.core.IIncoming;
+import de.tum.in.i22.pdp.core.PdpHandlerMock;
+import de.tum.in.i22.pdp.core.PdpHandlerMockLinux;
+import de.tum.in.i22.pdp.core.PdpHandlerPdpNative;
 import de.tum.in.i22.pdp.core.PdpHandlerTestPip;
 
 public class PdpModuleMockTestPip extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		//bind(IIncoming.class).to(PdpHandlerMockLinux.class).in(Singleton.class);
-		
+//		bind(IIncoming.class).to(PdpHandlerMockLinux.class).in(Singleton.class);
+
 		bind(IIncoming.class).to(PdpHandlerTestPip.class).in(Singleton.class);
 	}
 
