91c496a8ff01733b489d954fa3bb82d56fb64d32
Florian Kelbert <kelbert@cs.tum.edu>
2014-02-21 13:46:45 +0100

    toString() for class Name

--- PipCore/src/main/java/de/tum/in/i22/pip/core/Name.java
+++ PipCore/src/main/java/de/tum/in/i22/pip/core/Name.java
@@ -1,19 +1,24 @@
 package de.tum.in.i22.pip.core;
 
 /**
- * A container can have several representations. 
+ * A container can have several representations.
  * PipName is a representation of the container.
  * @author Stoimenov
  *
  */
 public class Name {
 	public String _name;
-	
+
 	public Name(String name) {
 		this._name = name;
 	}
-	
+
 	public String getName() {
+		return _name;
+	}
+
+	@Override
+	public String toString() {
 		return _name;
 	}
 }
