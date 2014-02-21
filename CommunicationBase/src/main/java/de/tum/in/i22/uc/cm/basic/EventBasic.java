b7e4a77da9c287d500cc48486404bca0eb24d813
Florian Kelbert <kelbert@cs.tum.edu>
2014-02-21 13:52:55 +0100

    Fixed NullPointer in EventBasic

--- CommunicationBase/src/main/java/de/tum/in/i22/uc/cm/basic/EventBasic.java
+++ CommunicationBase/src/main/java/de/tum/in/i22/uc/cm/basic/EventBasic.java
@@ -11,11 +11,11 @@ import de.tum.in.i22.uc.cm.gpb.PdpProtos.GpEvent;
 import de.tum.in.i22.uc.cm.gpb.PdpProtos.GpEvent.GpMapEntry;
 
 public class EventBasic implements IEvent {
-	
+
 	public static final String PEP_PARAMETER_KEY = "PEP";
-	
+
 	public static final String PREFIX_SEPARATOR = "_";
-	
+
 	private String _name = null;
 	private String _pep = null;
 	private boolean _isActual = false;
@@ -25,28 +25,28 @@ public class EventBasic implements IEvent {
 	public EventBasic() {
 		_map = new HashMap<>();
 	}
-	
+
 	public EventBasic(String name, Map<String, String> map) {
 		_name = name;
 		_map = map;
-		_pep = map.get(PEP_PARAMETER_KEY);
+		setPep(map);
 	}
-	
+
 	public EventBasic(String name, Map<String, String> map, boolean isActual) {
 		this(name, map);
 		_isActual = isActual;
 	}
-	
+
 	public EventBasic(GpEvent gpEvent) {
 		if (gpEvent == null)
 			return;
-		
+
 		if (gpEvent.hasName())
 			_name = gpEvent.getName();
-		
+
 		if (gpEvent.hasIsActual())
 			_isActual = gpEvent.getIsActual();
-		
+
 		//number of elements in the map
 		int count = gpEvent.getMapEntryCount();
 		if (count > 0) {
@@ -58,7 +58,7 @@ public class EventBasic implements IEvent {
 			}
 		}
 
-		_pep = _map.get(PEP_PARAMETER_KEY);
+		setPep(_map);
 
 		// Insert timestamp
 		if (gpEvent.hasTimestamp() && gpEvent.getTimestamp() != null && !gpEvent.getTimestamp().isEmpty())
@@ -66,29 +66,36 @@ public class EventBasic implements IEvent {
 		else
 			_timestamp = 0;
 	}
-	
+
 	public void addParameter(String key, String value) {
 		_map.put(key, value);
 	}
-	
+
 	@Override
 	public long getTimestamp() {
 		return _timestamp;
 	}
-	
+
 	@Override
 	public String getPrefixedName() {
 		if (_pep == null) {
 			return _name;
 		}
-		
+
 		return _pep + PREFIX_SEPARATOR + _name;
 	}
 
+	@Override
 	public String getName() {
 		return _name;
 	}
-	
+
+	private void setPep(Map<String,String> parameters) {
+		if (parameters != null) {
+			_pep = parameters.get(PEP_PARAMETER_KEY);
+		}
+	}
+
 	@Override
 	public String getPep() {
 		return _pep;
@@ -97,7 +104,7 @@ public class EventBasic implements IEvent {
 	public void setName(String name) {
 		_name = name;
 	}
-	
+
 	public void setActual(boolean isActual) {
 		_isActual = isActual;
 	}
@@ -106,31 +113,31 @@ public class EventBasic implements IEvent {
 	public boolean isActual() {
 		return _isActual;
 	}
-	
+
 	@Override
 	public Map<String, String> getParameters() {
 		return _map;
 	}
-	
+
 	public void setTimestamp(long timestamp) {
 		_timestamp = timestamp;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param e
 	 * @return Google Protocol Buffer object corresponding to IEvent
 	 */
 	public static GpEvent createGpbEvent(IEvent e) {
-		if (e == null) 
+		if (e == null)
 			return null;
-		
+
 		PdpProtos.GpEvent.Builder gpEvent = PdpProtos.GpEvent.newBuilder();
 		if (e.getPrefixedName() != null)
 			gpEvent.setName(e.getName());
-		
+
 		gpEvent.setIsActual(e.isActual());
-		
+
 		Map<String, String> map = e.getParameters();
 		if (map != null && !map.isEmpty()) {
 			Set<String> keys = map.keySet();
@@ -139,11 +146,11 @@ public class EventBasic implements IEvent {
 				PdpProtos.GpEvent.GpMapEntry.Builder entry = PdpProtos.GpEvent.GpMapEntry.newBuilder();
 				entry.setKey(key);
 				entry.setValue(value);
-				
+
 				gpEvent.addMapEntry(entry);
 			}
 		}
-		
+
 		return gpEvent.build();
 	}
 
@@ -164,5 +171,5 @@ public class EventBasic implements IEvent {
 					&& CompareUtil.areMapsEqual(_map, o.getParameters());
 		}
 		return isEqual;
-	}	
+	}
 }
