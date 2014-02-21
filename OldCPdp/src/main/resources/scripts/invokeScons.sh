22f2f91f3769add89f5b7f1c5b78a2b3dec90a28
lovat <lovat@cs.tum.edu>
2014-02-21 13:28:08 +0100

    Deleted old C Pdp

--- OldCPdp/src/main/resources/scripts/invokeScons.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-command -v scons >/dev/null 2>&1 || { echo >&2 "scons is not installed. using preinstalled libraries."; mkdir -p ../../../target/classes/; cp -r ../../../nativeLibs ../../../target/classes/; exit 0; }
-scons $@
