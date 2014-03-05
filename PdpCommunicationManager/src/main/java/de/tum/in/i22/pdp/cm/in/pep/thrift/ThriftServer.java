6627664f814e6ff617e40c29e59d07667b33aefe
Florian Kelbert <kelbert@cs.tum.edu>
2014-03-05 06:45:06 +0100

    ThriftServer now using proper Logger class. Also added Generic type safety.

--- PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/pep/thrift/ThriftServer.java
+++ PdpCommunicationManager/src/main/java/de/tum/in/i22/pdp/cm/in/pep/thrift/ThriftServer.java
@@ -1,5 +1,6 @@
 package de.tum.in.i22.pdp.cm.in.pep.thrift;
 
+import org.apache.log4j.Logger;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TServer.Args;
 import org.apache.thrift.server.TSimpleServer;
@@ -8,11 +9,14 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
 
 public class ThriftServer {
-	private static TServer server=null;
-	
+	protected static Logger _logger = Logger.getLogger(ThriftServer.class);
+
+	private static TServer server = null;
+
 	public static void createListener(int thriftServerPort, int pepPort){
 		ThriftServerHandler handler = new ThriftServerHandler(pepPort);
-		ExtendedThriftConnector.Processor processor = new ExtendedThriftConnector.Processor(handler);
+		ExtendedThriftConnector.Processor<ThriftServerHandler> processor =
+				new ExtendedThriftConnector.Processor<ThriftServerHandler>(handler);
 		TServerTransport serverTransport;
 		try {
 			serverTransport = new TServerSocket(thriftServerPort);
@@ -21,17 +25,16 @@ public class ThriftServer {
 			e.printStackTrace();
 		}
 		server = new TSimpleServer(new Args(serverTransport).processor(processor));
-			
-		System.out.println("Server initialized on port "+thriftServerPort);
+
+		_logger.info("Server ThriftServer listening on port: "+thriftServerPort);
 	}
 
 	public static void start(){
-		System.out.println("Starting the simple server...");
 		server.serve();
 	}
-	
+
 	public static void stop(){
 		server.stop();
 	}
-	
+
 }
