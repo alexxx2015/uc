493568d0b8aaf71a5a7d926cab84863f18422e7e
Florian Kelbert <kelbert@cs.tum.edu>
2014-03-07 11:15:43 +0100

    * Added openat() handler * tweaks to open() and close() handlers

--- PipCore/src/main/java/de/tum/in/i22/pip/core/eventdef/Linux/OpenAtEventHandler.java
+++ PipCore/src/main/java/de/tum/in/i22/pip/core/eventdef/Linux/OpenAtEventHandler.java
@@ -2,6 +2,7 @@ package de.tum.in.i22.pip.core.eventdef.Linux;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import de.tum.in.i22.pip.core.eventdef.BaseEventHandler;
 import de.tum.in.i22.pip.core.eventdef.ParameterNotFoundException;
@@ -15,36 +16,70 @@ import de.tum.in.i22.uc.cm.datatypes.IStatus;
  * @author Florian Kelbert
  *
  */
-public class OpenEventHandler extends BaseEventHandler {
+public class OpenAtEventHandler extends BaseEventHandler {
 
 	@Override
 	public IStatus execute() {
 		String host = null;
 		String pid = null;
-		String fd = null;
+		String newfd = null;
+		String dirfd = null;
 		String filename = null;
+		boolean at_fdcwd = false;
 		boolean truncate = false;
 
 		try {
 			host = getParameterValue("host");
 			pid = getParameterValue("pid");
-			fd = getParameterValue("fd");
+			newfd = getParameterValue("newfd");
+			dirfd = getParameterValue("dirfd");
 			filename = getParameterValue("filename");
 			truncate = Boolean.valueOf(getParameterValue("trunc"));
+			at_fdcwd = Boolean.valueOf(getParameterValue("at_fdcwd"));
 		} catch (ParameterNotFoundException e) {
 			_logger.error(e.getMessage());
 			return _messageFactory.createStatus(EStatus.ERROR_EVENT_PARAMETER_MISSING, e.getMessage());
 		}
-
-		// get filedescriptor and filename
-		IName fdName = FiledescrName.create(host, pid, fd);
+		
+		IName newfdName = FiledescrName.create(host, pid, newfd);
+		IName dirfdName = FiledescrName.create(host, pid, dirfd);
 		IName fnName;
-		try {
-			fnName = FilenameName.create(host, new File(filename).getCanonicalPath());
-		} catch (IOException e) {
-			fnName = FilenameName.create(host, new File(filename).getAbsolutePath());
+		
+		File file = new File(filename);
+		
+		if (!file.isAbsolute() && !at_fdcwd) {
+			List<IName> names = ifModel.getAllNames(dirfdName, FilenameName.class);
+			
+			// the resulting list should always be of size 1.
+			if (names.size() != 1) {
+				_logger.error("There was not exactly one filename for " + dirfdName);
+			}
+			else {
+				File path = new File(((FilenameName) names.get(0)).getFilename());
+				
+				String pathStr;
+				try {
+					pathStr = path.getCanonicalPath();
+				} catch (IOException e) {
+					pathStr = path.getAbsolutePath();
+				}
+				
+				if (path.isDirectory()) {
+					file = new File(pathStr, filename);
+				}
+				else {
+					file = new File(path.getParent(), filename);
+				}
+			}
 		}
-
+		
+		try {
+			fnName = FilenameName.create(host, file.getCanonicalPath());
+		} catch (IOException e) {
+			fnName = FilenameName.create(host, file.getAbsolutePath());
+		}
+		
+	
 		// get the file's container (if present)
 		IContainer cont = ifModel.getContainer(fnName);
 
@@ -57,7 +92,7 @@ public class OpenEventHandler extends BaseEventHandler {
 			cont = _messageFactory.createContainer();
 			ifModel.addName(fnName, cont);
 		}
-		ifModel.addName(fdName, cont);
+		ifModel.addName(newfdName, cont);
 
 		return _messageFactory.createStatus(EStatus.OKAY);
 	}
