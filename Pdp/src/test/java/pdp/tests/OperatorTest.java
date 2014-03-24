3d26b171207be9af377959b483b217e57049edf0
Florian Kelbert <kelbert@cs.tum.edu>
2014-03-24 11:14:50 +0100

    impport

--- Pdp/src/test/java/pdp/tests/OperatorTest.java
+++ Pdp/src/test/java/pdp/tests/OperatorTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
