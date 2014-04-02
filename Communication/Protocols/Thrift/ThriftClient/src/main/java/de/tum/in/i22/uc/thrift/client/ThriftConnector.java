4d192af819e170d4cede7249fbd8128e04115a4f
Florian Kelbert <kelbert@cs.tum.edu>
2014-04-02 16:33:01 +0200

    Finally3

---
+++ Communication/Protocols/Thrift/ThriftClient/src/main/java/de/tum/in/i22/uc/thrift/client/ThriftConnector.java
@@ -0,0 +1,65 @@
+package de.tum.in.i22.uc.thrift.client;
+
+import org.apache.thrift.TServiceClient;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.tum.in.i22.uc.cm.client.TcpConnector;
+
+/**
+ * A class representing a {@link ThriftConnector}, i.e. a {@link TcpConnector}
+ * that will connect to a remote Thrift server.
+ *
+ * @author Florian Kelbert
+ *
+ */
+public class ThriftConnector<HandleType extends TServiceClient> extends TcpConnector<HandleType> {
+	protected static final Logger _logger = LoggerFactory.getLogger(ThriftConnector.class);
+
+	private TTransport _transport;
+	private final Class<HandleType> _iface;
+
+	/**
+	 * Creates a new {@link ThriftConnector} that will connect to the
+	 * specified address and port using the specified Thrift interface.
+	 *
+	 * @param address the address of the remote thrift server to connect to, such
+	 * 			as an IP address or a URL
+	 * @param port the server's port
+	 * @param iface the Thrift interface to be used for the connection
+	 */
+	public ThriftConnector(String address, int port, Class<HandleType> iface) {
+		super(address, port);
+		_iface = iface;
+	}
+
+
+	@Override
+	public HandleType connect() throws Exception {
+		HandleType handle = null;
+		_transport = new TSocket(_address, _port);
+
+		try {
+			_transport.open();
+			handle = _iface.getConstructor(TProtocol.class).newInstance(new TBinaryProtocol(_transport));
+		} catch (Exception e) {
+			_logger.debug("Failed to establish connection.", e);
+			disconnect();
+			throw e;
+		}
+
+		return handle;
+	}
+
+	@Override
+	public void disconnect() {
+		if (_transport != null) {
+			_transport.close();
+			_transport = null;
+		}
+	}
+}
---
+++ Communication/Protocols/Thrift/ThriftClient/src/main/java/de/tum/in/i22/uc/thrift/client/ThriftPdpClientHandler.java
@@ -0,0 +1,107 @@
+package de.tum.in.i22.uc.thrift.client;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.tum.i22.in.uc.thrift.types.TAny2Pdp;
+import de.tum.in.i22.uc.cm.client.PdpClientHandler;
+import de.tum.in.i22.uc.cm.datatypes.IEvent;
+import de.tum.in.i22.uc.cm.datatypes.IMechanism;
+import de.tum.in.i22.uc.cm.datatypes.IPxpSpec;
+import de.tum.in.i22.uc.cm.datatypes.IResponse;
+import de.tum.in.i22.uc.cm.datatypes.IStatus;
+import de.tum.in.i22.uc.cm.server.PdpProcessor;
+import de.tum.in.i22.uc.thrift.ThriftConverter;
+
+
+/**
+ * The client side of a remote Thrift {@link PdpProcessor} server.
+ *
+ * Create a instance of this class, connect it
+ * (using {@link PdpClientHandler#connect()}) and
+ * do calls on a remote {@link PdpProcessor}.
+ *
+ * @author Florian Kelbert
+ *
+ */
+public class ThriftPdpClientHandler extends PdpClientHandler<TAny2Pdp.Client> {
+	protected static final Logger _logger = LoggerFactory.getLogger(ThriftPdpClientHandler.class);
+
+	/**
+	 * Creates a {@link ThriftPdpClientHandler} that will be
+	 * connected (upon calling {@link PdpClientHandler#connect()})
+	 * the the specified thrift server on the specified address/port.
+	 *
+	 * @param address the address of the remote point
+	 * @param port the port of the remote point
+	 */
+	public ThriftPdpClientHandler(String address, int port) {
+		super(new ThriftConnector<>(address, port, TAny2Pdp.Client.class));
+	}
+
+	@Override
+	public IStatus deployMechanism(IMechanism mechanism) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IMechanism exportMechanism(String par) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatus revokeMechanism(String policyName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatus revokeMechanism(String policyName, String mechName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatus deployPolicy(String policyFilePath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, ArrayList<IMechanism>> listMechanisms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean registerPxp(IPxpSpec pxp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public IResponse notifyEventAsync(IEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IResponse notifyEventSync(IEvent event) {
+		try {
+			return ThriftConverter.fromThrift(
+					_handle.
+					notifyEventSync(
+							ThriftConverter.
+							toThrift(event)));
+		} catch (TException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+}
