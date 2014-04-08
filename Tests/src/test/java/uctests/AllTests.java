2602c3ddd883f5581ea25665e7157503e1361c6f
Florian Kelbert <kelbert@cs.tum.edu>
2014-04-08 18:18:36 +0200

    fixed pom

--- Tests/src/test/java/uctests/AllTests.java
+++ Tests/src/test/java/uctests/AllTests.java
@@ -8,10 +8,6 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@RunWith(Suite.class)
-
-//@SuiteClasses({  })
-
 public class AllTests {
 
 	private static Logger _logger = LoggerFactory.getLogger(AllTests.class);
