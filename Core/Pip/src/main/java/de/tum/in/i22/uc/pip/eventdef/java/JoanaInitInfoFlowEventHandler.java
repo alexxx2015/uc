c680806c3d6d7e56809814ca865510feb6a10539
user <user@uservm.(none)>
2014-07-22 16:27:00 +0200

    fixed bug in joanainit

--- Core/Pip/src/main/java/de/tum/in/i22/uc/pip/eventdef/java/JoanaInitInfoFlowEventHandler.java
+++ Core/Pip/src/main/java/de/tum/in/i22/uc/pip/eventdef/java/JoanaInitInfoFlowEventHandler.java
@@ -78,9 +78,11 @@ public class JoanaInitInfoFlowEventHandler extends JavaEventHandler {
 			JSONObject o = listOfFlowIt.next();
 			String sink = (String) o.get("sink");
 			JSONArray sources = (JSONArray)o.get("sources");
-			String[] s = new String[sources.size()]; 
-			sources.toArray(s);
-			iFlow.put(pid+_javaIFDelim+sink.toLowerCase(), s);
+			String[] toBeAdded = new String[sources.size()]; //first element is sink
+			for (int i=0; i<sources.size(); i++){
+				toBeAdded[i]=pid+_javaIFDelim+((String)(sources.get(i))).toLowerCase();
+			}
+			iFlow.put(pid+_javaIFDelim+sink.toLowerCase(), toBeAdded);
 		}
 		
 //		parseList(listOfSources, pid, "source", sep1, sep2);
