964ff5eab26562b740d5300c256424eb752bf876
Florian Kelbert <kelbert@cs.tum.edu>
2014-08-15 15:24:20 +0200

    did not compile on linux and fixed some dependencies

--- Core/Ptp/src/main/java/de/tum/in/i22/uc/policy/translation/ecacreation/JPanelDelayAction.java
+++ Core/Ptp/src/main/java/de/tum/in/i22/uc/policy/translation/ecacreation/JPanelDelayAction.java
@@ -1,21 +1,16 @@
 package de.tum.in.i22.uc.policy.translation.ecacreation;
 
-import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import javax.swing.JComboBox;
-import java.awt.Insets;
-import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
 import javax.swing.DefaultComboBoxModel;
-import net.miginfocom.swing.MigLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.factories.FormFactory;
-import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class JPanelDelayAction extends JPanel implements ECAAction{
 	private JTextField jtfTimeValue;
@@ -31,7 +26,7 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblTimeUnit = new JLabel("Time Unit");
 		lblTimeUnit.setFont(new Font("Tahoma", Font.BOLD, 11));
 		GridBagConstraints gbc_lblTimeUnit = new GridBagConstraints();
@@ -41,7 +36,7 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 		gbc_lblTimeUnit.gridx = 0;
 		gbc_lblTimeUnit.gridy = 1;
 		add(lblTimeUnit, gbc_lblTimeUnit);
-		
+
 		jcbTimeUnit = new JComboBox();
 		jcbTimeUnit.setModel(new DefaultComboBoxModel(new String[] {"Timesteps", "Nanoseconds", "Microseconds", "Milliseconds", "Seconds", "Minutes", "Hours", "Days", "Weeks", "Months", "Years"}));
 		GridBagConstraints gbc_jcbTimeUnit = new GridBagConstraints();
@@ -51,7 +46,7 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 		gbc_jcbTimeUnit.gridx = 3;
 		gbc_jcbTimeUnit.gridy = 1;
 		add(jcbTimeUnit, gbc_jcbTimeUnit);
-		
+
 		JLabel lblTimeValue = new JLabel("Time Value");
 		lblTimeValue.setFont(new Font("Tahoma", Font.BOLD, 11));
 		GridBagConstraints gbc_lblTimeValue = new GridBagConstraints();
@@ -61,7 +56,7 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 		gbc_lblTimeValue.gridx = 0;
 		gbc_lblTimeValue.gridy = 2;
 		add(lblTimeValue, gbc_lblTimeValue);
-		
+
 		jtfTimeValue = new JTextField();
 		GridBagConstraints gbc_jtfTimeValue = new GridBagConstraints();
 		gbc_jtfTimeValue.insets = new Insets(0, 0, 0, 5);
@@ -75,23 +70,23 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 
 	@Override
 	public String getActionName() {
-		
+
 		return "delay";
 	}
 
 	@Override
 	public Object getActionResult() throws ECAInputException{
-				
-		ArrayList<Object> alResult=new ArrayList<Object>();				
+
+		ArrayList<Object> alResult=new ArrayList<Object>();
 		alResult.add(jcbTimeUnit.getSelectedItem().toString());
-		alResult.add(jtfTimeValue.getText());		
+		alResult.add(jtfTimeValue.getText());
 		return alResult;
 	}
 
 	@Override
 	public void populateUI(Object action) {
-		
-		if(action!=null){			
+
+		if(action!=null){
 			if(!(action instanceof String)){
 			@SuppressWarnings("unchecked")
 			ArrayList<Object> actionDetails=(ArrayList<Object>)action;
@@ -100,7 +95,7 @@ public class JPanelDelayAction extends JPanel implements ECAAction{
 		  }
 		}
 		else jtfTimeValue.setText("");
-		
+
 	}
 
 }
--- Core/Ptp/src/main/java/de/tum/in/i22/uc/policy/translation/ecacreation/JPanelExecuteAction.java
+++ Core/Ptp/src/main/java/de/tum/in/i22/uc/policy/translation/ecacreation/JPanelExecuteAction.java
@@ -1,17 +1,10 @@
 package de.tum.in.i22.uc.policy.translation.ecacreation;
 
 import javax.swing.DefaultListModel;
-import javax.swing.JDialog;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.ListModel;
-
 import java.awt.Container;
-import java.awt.GridBagLayout;
 import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
 import javax.swing.JButton;
-import java.awt.Insets;
 import javax.swing.JList;
 import java.awt.Font;
 import javax.swing.border.LineBorder;
@@ -22,15 +15,11 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.ArrayList;
 
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.factories.FormFactory;
-import com.jgoodies.forms.layout.RowSpec;
 import net.miginfocom.swing.MigLayout;
 
 public class JPanelExecuteAction extends JPanel implements ECAAction{
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -5848685299730507786L;
 	private JButton btnAdd;
@@ -50,7 +39,7 @@ public class JPanelExecuteAction extends JPanel implements ECAAction{
 		JLabel lblManageActions = new JLabel("Manage Actions");
 		lblManageActions.setFont(new Font("Tahoma", Font.BOLD, 11));
 		add(lblManageActions, "cell 0 0,alignx center,aligny center");
-		
+
 		lsActions = new JList();
 		lsActions.setBorder(new LineBorder(new Color(0, 0, 0)));
 		add(lsActions, "cell 0 1 1 6,grow");
@@ -58,112 +47,112 @@ public class JPanelExecuteAction extends JPanel implements ECAAction{
 
 			@Override
 			public void focusGained(FocusEvent fe) {
-				
-				
+
+
 			}
 
 			@Override
 			public void focusLost(FocusEvent fe) {
-								
+
 				//lsActions.getSelectionModel().clearSelection();
 			}
-			
+
 		});
-		
+
 		btnAdd = new JButton("Add");
 		add(btnAdd, "cell 1 1,growx,aligny center");
 		btnAdd.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent ae) {
-				
-				
+
+
 				JDialogEAParameter.launch("Action", -1, controller, container, JPanelExecuteAction.this);
 				lsActions.getSelectionModel().clearSelection();
 			}
-			
+
 		});
-		
+
 		btnEdit = new JButton("Edit");
 		add(btnEdit, "cell 1 3,growx");
 		btnEdit.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent ae) {
-				
+
 				int iSel=-1;
 				iSel=lsActions.getSelectedIndex();
 				if(iSel>-1 && controller.getExecuteActions().size()>0){
 					JDialogEAParameter.launch("Action", iSel, controller, container, JPanelExecuteAction.this);
 				}
-				
+
 				lsActions.getSelectionModel().clearSelection();
 			}
-			
+
 		});
-		
+
 		btnDelete = new JButton("Delete");
 		add(btnDelete, "cell 1 6,alignx center,aligny center");
 		btnDelete.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent ae) {
-				
+
 				int iSel=lsActions.getSelectedIndex();
 				if(iSel!=-1){
 					if(controller.getExecuteActions().size()>0){
-						controller.getExecuteActions().remove(iSel);					
-					}				
+						controller.getExecuteActions().remove(iSel);
+					}
 				}
 				showExecuteActions();
 			}
-			
+
 		});
 
 	}
-	
+
 	public void setParentContainer(Container parent){
 		this.container=parent;
 	}
-	
+
 	public void setController(ECARulesCreatorController controller){
 		this.controller=controller;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void showExecuteActions(){
-		
+
 		ArrayList<SubformulaEvent> alEvents=controller.getExecuteActions();
 		DefaultListModel dlModel=new DefaultListModel();
-		if(alEvents.size()>0){									
+		if(alEvents.size()>0){
 			for(int i=0; i<alEvents.size(); ++i){
 				dlModel.add(i, alEvents.get(i).getEventName());
-			}			
+			}
 		}
 		lsActions.setModel(dlModel);
 	}
-	
+
 	public Object getView(){
 		return dlgParent;
 	}
 
 	@Override
 	public String getActionName() {
-		
+
 		return "execute";
 	}
 
 	@Override
 	public Object getActionResult() throws ECAInputException {
-		
+
 		return controller.getExecuteActions();
 	}
 
 	@Override
 	public void populateUI(Object action) {
-		
+
 		if(action!=null){
-			
+
 		}
 	}
 
