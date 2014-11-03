3134cdfbdf51c9590269ae1be49f5e4e33e8cb85
Florian Kelbert <kelbert@cs.tum.edu>
2014-11-03 17:39:57 +0100

    ConditionParamMatchOperator is back. Not tested.

--- Core/Pdp/src/main/java/de/tum/in/i22/uc/pdp/core/operators/ConditionParamMatchOperator.java
+++ Core/Pdp/src/main/java/de/tum/in/i22/uc/pdp/core/operators/ConditionParamMatchOperator.java
@@ -1,11 +1,20 @@
 package de.tum.in.i22.uc.pdp.core.operators;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import com.google.common.base.MoreObjects;
 
+import de.tum.in.i22.uc.cm.datatypes.interfaces.IEvent;
 import de.tum.in.i22.uc.pdp.core.Mechanism;
+import de.tum.in.i22.uc.pdp.core.ParamMatch;
+import de.tum.in.i22.uc.pdp.core.PolicyDecisionPoint;
+import de.tum.in.i22.uc.pdp.core.operators.State.StateVariable;
 import de.tum.in.i22.uc.pdp.xsd.ConditionParamMatchType;
 
-public class ConditionParamMatchOperator extends ConditionParamMatchType {
+public class ConditionParamMatchOperator extends ConditionParamMatchType implements Observer {
+
+	private ParamMatch pm;
 
 	public ConditionParamMatchOperator() {
 	}
@@ -13,6 +22,23 @@ public class ConditionParamMatchOperator extends ConditionParamMatchType {
 	@Override
 	protected void init(Mechanism mech, Operator parent, long ttl) {
 		super.init(mech, parent, ttl);
+
+		pm = new ParamMatch();
+		pm.setName(getName());
+		pm.setValue(getValue());
+		pm.setCmpOp(getCmpOp());
+
+		_pdp.addObserver(this);
+	}
+
+	@Override
+	public void startSimulation() {
+		super.startSimulation();
+	}
+
+	@Override
+	public void stopSimulation() {
+		super.stopSimulation();
 	}
 
 	@Override
@@ -26,6 +52,26 @@ public class ConditionParamMatchOperator extends ConditionParamMatchType {
 
 	@Override
 	public boolean tick() {
-		return false;
+		/*
+		 * Lookup whether the last event parameter matched.
+		 */
+		boolean value = _state.get(StateVariable.SINCE_LAST_TICK);
+		_state.set(StateVariable.VALUE_AT_LAST_TICK, value);
+		return value;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (o instanceof PolicyDecisionPoint && arg instanceof IEvent) {
+
+			boolean matches = pm.matches(pm.getName(), ((IEvent) arg).getParameterValue(pm.getName()));
+
+			/*
+			 * Always overwrite SINCE_LAST_TICK, because we are only
+			 * interested in the last event happening, i.e. the event
+			 * happening NOW.
+			 */
+			_state.set(StateVariable.SINCE_LAST_TICK, matches);
+		}
 	}
 }
