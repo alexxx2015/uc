65626418c7147a62dcbe2f5ddb6939b311881f12
Florian Kelbert <kelbert@cs.tum.edu>
2015-03-30 17:54:05 +0200

    Removed parallelism when deploying policies that caused trouble

--- Core/Pdp/src/main/java/de/tum/in/i22/uc/pdp/core/MechanismFactory.java
+++ Core/Pdp/src/main/java/de/tum/in/i22/uc/pdp/core/MechanismFactory.java
@@ -3,13 +3,10 @@ package de.tum.in.i22.uc.pdp.core;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorCompletionService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.tum.in.i22.uc.cm.distribution.Threading;
 import de.tum.in.i22.uc.pdp.core.exceptions.InvalidMechanismException;
 import de.tum.in.i22.uc.pdp.core.exceptions.InvalidPolicyException;
 import de.tum.in.i22.uc.pdp.xsd.DetectiveMechanismType;
@@ -28,11 +25,8 @@ class MechanismFactory {
 
 	private final PolicyDecisionPoint _pdp;
 
-	private final CompletionService<?> _cs;
-
 	MechanismFactory(PolicyDecisionPoint pdp) {
 		_pdp = pdp;
-		_cs = new ExecutorCompletionService<>(Threading.instance());
 	}
 
 	private Mechanism create(MechanismBaseType mech, String policyName) throws InvalidMechanismException {
@@ -53,14 +47,13 @@ class MechanismFactory {
 
 		List<MechanismBaseType> mechs = policy.getDetectiveMechanismOrPreventiveMechanism();
 
-		mechs.forEach(m -> _cs.submit(() -> {
+		for (MechanismBaseType m : mechs) {
 			try {
 				mechanisms.add(create(m, policy.getName()));
 			} catch (InvalidMechanismException e) {
 				_logger.error("Invalid mechanism specified: {} ({})", m.getName(), e.getMessage());
 			}
-		}, null));
-		Threading.waitFor(mechs.size(), _cs);
+		}
 
 		if (mechanisms.size() != mechs.size()) {
 			_logger.error("Policy {} is invalid, because at least one of its mechanisms is invalid.", policy.getName());
